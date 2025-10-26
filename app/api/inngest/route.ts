4be22c4fd9ae785343c65f19402d6287d3410cd5
lighting.launch <lighting.launch@gmail.com>
2025-10-26 05:32:25 +0000

    ignore

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,4 +1,6 @@
 import { inngest } from "../../../lib/inngest/client";
 import { serve } from "inngest/next";
 
+export const runtime = 'edge';
+
 export const { GET, POST, PUT } = serve({ client: inngest, functions: [] });
